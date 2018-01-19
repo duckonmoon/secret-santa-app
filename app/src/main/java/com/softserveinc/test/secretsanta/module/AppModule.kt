54fcda570412f05896cfadd9bdbb78e891008d97
duckonmoon <rostyk1997@gmail.com>
2018-01-19 17:50:41 +0200

    add nothing

--- app/src/main/java/com/softserveinc/test/secretsanta/module/AppModule.kt
+++ app/src/main/java/com/softserveinc/test/secretsanta/module/AppModule.kt
@@ -2,6 +2,7 @@ package com.softserveinc.test.secretsanta.module
 
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.softserveinc.test.secretsanta.controller.MainController
 import dagger.Module
 import dagger.Provides
@@ -9,10 +10,19 @@ import javax.inject.Singleton
 
 @Module
 class AppModule {
+
+    private val database : FirebaseDatabase = FirebaseDatabase.getInstance()!!
+
     @Provides
     @Singleton
     fun provideApp() : MainController = MainController.INSTANCE
 
     val auth = FirebaseAuth.getInstance()!!
+        @Provides @Singleton get
+
+    val databaseUsers = database.getReference("users")!!
+        @Provides @Singleton get
+
+    val databaseGroups = database.getReference("groups")!!
         @Provides @Singleton get
 }
